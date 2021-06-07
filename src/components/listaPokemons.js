6788843d8b39633e692dc8c1cc3b60dfb57784e6
jhorman1999 <jhormandaniel1999@gmail.com>
2021-06-07 15:19:59 -0500

    argregar imagenes pokemon

--- src/components/listaPokemons.js
+++ src/components/listaPokemons.js
@@ -1,13 +1,23 @@
 import datos from '../data/data.json'
 import './listaPokemons.css'
 
-const TargetasPokemon =datos.map((datos) => (
+const TargetasPokemon = datos.map((datos) => (
     <div className="targetaPokemon">
-    <div className="datos">{datos['#']}  {datos['Name']}</div>
-
-    <img className='imagenLista'src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"+datos['#']+".png"}></img>
-    <br></br>
+        <div className="datos">{datos['#']}  {datos['Name']}</div>
+        <img className='imagenLista' src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + datos['#'] + ".png"}></img>
+        <div className="type">
+            <div className={datos['Type 1']}>
+                {datos['Type 1']}
+            </div>
+            <div className={datos['Type 2']}>
+                {datos['Type 2']}
+            </div>
+            
+        </div>
     </div>
+
 ))
+
+
 
 export default TargetasPokemon
