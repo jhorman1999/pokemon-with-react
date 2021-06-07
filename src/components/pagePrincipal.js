71ca4cdc74465a551629312a32f7f47a74c3637a
jhorman1999 <jhormandaniel1999@gmail.com>
2021-06-07 12:08:03 -0500

    first commit

---
+++ src/components/pagePrincipal.js
@@ -0,0 +1,15 @@
+import Papa from 'papaparse';
+import datos from '../data/data.json'
+
+const Pokemos = () => {
+    
+    console.log(datos)
+    return (
+        <div>
+        <h1>hola</h1>
+        </div>
+    )
+
+}
+
+export default (Pokemos)
